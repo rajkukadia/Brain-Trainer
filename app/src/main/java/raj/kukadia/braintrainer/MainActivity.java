231a23cd1cb8999831e5ffc0bab35f5f9dbe05e8
Raj Kukadia <rkukadia@intralinks.com>
2018-08-17 01:19:51 -0400

    logic

--- app/src/main/java/raj/kukadia/braintrainer/MainActivity.java
+++ app/src/main/java/raj/kukadia/braintrainer/MainActivity.java
@@ -2,33 +2,59 @@ package raj.kukadia.braintrainer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startButton;
+    int locationOfAnswer;
+    int incorrectAns;
+    ArrayList<Integer>  answers;
+
+
+    public void chooseAnswer(View v){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        startButton = findViewById(R.id.start);
         TextView timmer = findViewById(R.id.timmer);
-        timmer.setText("00:00");
         TextView score = findViewById(R.id.score);
-        score.setText("1/1");
-        GridView grid = findViewById(R.id.grid);
-        ArrayList<String> list = new ArrayList<String>();
-        list.add("a");
-        list.add("b");
-        list.add("c");
-        list.add("d");
+        TextView equation = findViewById(R.id.equation);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
 
-        grid.setAdapter(adapter);
+        Random rand = new Random();
+
+        int a = rand.nextInt(21);
+        int b = rand.nextInt(21);
+
+        equation.setText(Integer.toString(a) + "+" + Integer.toString(b) );
+
+        locationOfAnswer = rand.nextInt(4);
+        incorrectAns = rand.nextInt(41);
+
+        for(int i = 0; i<4; i++){
+            if(i == locationOfAnswer){
+                answers.add(a+b);
+            }else{
+                while(incorrectAns == a+b){
+                    incorrectAns = rand.nextInt(41);
+                }
+                answers.add(incorrectAns);
+            }
+        }
+
 
     }
 }
